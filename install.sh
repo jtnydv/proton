351db3e4756ecd8575f044752cab59da47059d3c
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-08-19 15:54:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations under
 # the License.
-
+#
 # Disclaimer:
 # Usage of Entypreter for attacking targets without prior mutual consent is illegal.
 # It is the end user's responsibility to obey all applicable local, state,
