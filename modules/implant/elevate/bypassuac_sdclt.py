8dd41b4ece93415e02014d428c55547a47b4386e
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-08-21 20:39:51 +0200

    Update bypassuac_sdclt.py

--- modules/implant/elevate/bypassuac_sdclt.py
+++ modules/implant/elevate/bypassuac_sdclt.py
@@ -15,7 +15,7 @@ class SDCLTJob(core.job.Job):
         self.display()
 
     def display(self):
-        self.results = "Completed"
+        self.results = "Completed!"
         #self.shell.print_plain(self.data)
 
 class SDCLTImplant(core.implant.Implant):
