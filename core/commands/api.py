33125392a6422b1c845c78d257fe7288d0a5d0b1
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-10 20:30:14 +0300

    Update api.py

--- core/commands/api.py
+++ core/commands/api.py
@@ -11,11 +11,10 @@ def autocomplete(shell, line, text, state):
 def help(shell):
     shell.print_plain("")
     shell.print_plain("Turning on the REST Server:")
-    shell.print_plain("api on (--user USERNAME --pass PASSWORD --port PORT)")
-    shell.print_plain("Username and password defaults to 'entypreter'. Port defaults to 9990.")
+    shell.print_plain("usage: api on --user USERNAME --pass PASSWORD --port PORT")
     shell.print_plain("")
     shell.print_plain("Turning off the REST Server:")
-    shell.print_plain("api off")
+    shell.print_plain("usage: api off")
     shell.print_plain("")
 
 def execute(shell, cmd):
