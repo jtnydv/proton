9b010cd8796eee2559e5c57887229f4b84090c62
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-04 22:43:08 +0300

    Update verbose.py

--- core/commands/verbose.py
+++ core/commands/verbose.py
@@ -1,4 +1,4 @@
-DESCRIPTION = "Turn verbosity on/off: verbose (on|off)."
+DESCRIPTION = "Turn verbosity on/off."
 
 def autocomplete(shell, line, text, state):
     return None
@@ -11,6 +11,10 @@ def execute(shell, cmd):
 
     if len(splitted) > 1:
         sw = splitted[1].lower()
+        if sw == "":
+            shell.print_plain("")
+            shell.print_plain("Usage: verbose [on|off]")
+            shell.print_plain("")
         if sw == "1" or sw == "true" or sw == "on":
             shell.verbose = True
         else:
