e3f622905419cdc01c388326a2ea1b36b30be4c1
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-05 01:03:08 +0300

    Update jobs.py

--- core/commands/jobs.py
+++ core/commands/jobs.py
@@ -42,7 +42,10 @@ def execute(shell, cmd):
         print_job(shell, id)
         return
 
-    print_all_jobs(shell)
-    shell.print_plain("")
-    shell.print_plain("Use "jobs %s" to view job results (if any)." % (shell.colors.colorize("ID", shell.colors.BOLD)))
-    shell.print_plain("")
+    if shell.jobs:
+        print_all_jobs(shell)
+        shell.print_plain("")
+        shell.print_plain("Use "jobs %s" to view job results (if any)." % (shell.colors.colorize("ID", shell.colors.BOLD)))
+        shell.print_plain("")
+    else:
+        shell.print_error("No active jobs yet.")
