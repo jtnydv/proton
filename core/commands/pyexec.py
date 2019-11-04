d227b935f2189f7ff6dbf0e23decfe61db73f158
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-04 23:25:39 +0300

    Update pyexec.py

--- core/commands/pyexec.py
+++ core/commands/pyexec.py
@@ -4,7 +4,9 @@ def autocomplete(shell, line, text, state):
     return None
 
 def help(shell):
-    pass
+    shell.print_plain("")
+    shell.print_plain("Usage: pyexec <code>")
+    shell.print_plain("")
 
 def execute(shell, cmd):
     code = " ".join(cmd.split(" ")[1:])
