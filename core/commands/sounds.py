99706b74a92fc5231a141502af9c3429de198fad
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-04 22:59:12 +0300

    Update sounds.py

--- core/commands/sounds.py
+++ core/commands/sounds.py
@@ -19,15 +19,13 @@ def execute(shell, cmd):
 
     if len(splitted) > 1:
         sw = splitted[1].lower()
-        if sw == "":
-            shell.print_plain("")
-            shell.print_plain("Usage: sounds [on|off]")
-            shell.print_plain("")
         if sw == "1" or sw == "true" or sw == "on":
             from core.sounds import sounds
             shell.sounds = sounds
             shell.play_sound('ON')
-        else:
+            shell.print_status("Sounds: %s" % ("on" if shell.sounds else "off"))
+        if sw == "0" or sw == "false" or sw == "off":
             shell.sounds = {}
-
-    shell.print_status("Sounds: %s" % ("on" if shell.sounds else "off"))
+            shell.print_status("Sounds: %s" % ("on" if shell.sounds else "off"))
+    
+    help(shell)
