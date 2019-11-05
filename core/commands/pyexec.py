297b3b1f1a56a3dd45352cc992483d95167c95b4
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-05 09:10:01 +0000

    Update pyexec.py

--- core/commands/pyexec.py
+++ core/commands/pyexec.py
@@ -9,9 +9,9 @@ def help(shell):
     shell.print_plain("")
 
 def execute(shell, cmd):
-     if len(splitted) > 1:
-            code = " ".join(cmd.split(" ")[1:])
-            exec(code)
-            else:
-                help(shell)
+    if len(splitted) > 1:
+        code = " ".join(cmd.split(" ")[1:])
+        exec(code)
+        else:
+            help(shell)
     
