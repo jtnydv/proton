51918afb22589c956bef089236496637880db169
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-07 21:24:14 +0300

    Update download_file.py

--- modules/implant/util/download_file.py
+++ modules/implant/util/download_file.py
@@ -7,7 +7,7 @@ import os
 class DownloadFileImplant(core.implant.Implant):
 
     NAME = "Download File"
-    DESCRIPTION = "Downloads a file from the target machine."
+    DESCRIPTION = "Downloads a file from the zombie."
     AUTHORS = ["Entynetproject"]
     STATE = "implant/util/download_file"
 
