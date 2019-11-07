0927e0366392acf9d3110af315e53dd02971b219
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-07 21:25:30 +0300

    Update shell.py

--- core/commands/shell.py
+++ core/commands/shell.py
@@ -29,7 +29,7 @@ def get_prompt(shell, id, ip, cwd, isreadline = True):
 def cmdshell_help(shell):
     shell.print_plain("")
     shell.print_plain('Use "cd %s" to change current working directory.' % (shell.colors.colorize("PATH", shell.colors.BOLD)))
-    shell.print_plain('Use "upload %s" to upload a local file to zombie.' % (shell.colors.colorize("LFILE", shell.colors.BOLD)))
+    shell.print_plain('Use "upload %s" to upload a local file to the zombie.' % (shell.colors.colorize("LFILE", shell.colors.BOLD)))
     shell.print_plain('Use "download %s" to download a file from the zombie.' % (shell.colors.colorize("FILE", shell.colors.BOLD)))
     shell.print_plain('Use "help" to show full list of all available commands.')
     shell.print_plain('Use "exit" to leave this shell and return to Entypreter.')    
