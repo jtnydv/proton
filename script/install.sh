63e931be1dce707a09b208716524c3596c943eaf
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-09 23:52:23 +0300

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -7,7 +7,7 @@ CE="\033[0m"
 if [[ $EUID -ne 0 ]]
 then
    sleep 1
-   echo -e ""$RS"[-] "$WHS"This script must be run as root" 1>&2
+   echo -e ""$RS"[-] "$WHS"This script must be run as root!" 1>&2
    sleep 1
    exit
 fi
