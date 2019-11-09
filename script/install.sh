a5141e5a35c01f89da0392553b8c4b49c08cce4c
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-09 14:58:05 -0500

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
+RS="\033[31m"
+WHS="\033[0;97m"
+CE="\033[0m"
+
+if [[ $EUID -ne 0 ]]
+then
+   sleep 1
+   echo -e ""$RS"[-] "$WHS"This script must be run as root" 1>&2
+   sleep 1
+   exit
+fi
+
 if -d ~/proton; then
   sleep 0
 else
