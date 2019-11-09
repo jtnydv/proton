d752c55a04c059432c19ea8d12223e431fd18381
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-09 14:47:55 -0500

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -1,2 +1,15 @@
-if -d ~/proton;then
+if -d ~/proton; then
+  sleep 0
+else
+  cd ~
+  {
+  git clone https://github.com/entynetproject/proton.git
+  } &> /dev/null
+  cd ~/proton
+  chmod +x install.sh
+  ./install.sh
+fi
+
+{
+} &> /dev/null
   
