a847a3ff01d1786e9ef14dedb072ab0f8eb75c8b
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-10 23:02:53 +0300

    Update exit.py

--- core/commands/exit.py
+++ core/commands/exit.py
@@ -6,7 +6,9 @@ def autocomplete(shell, line, text, state):
     return None
 
 def help(shell):
-    pass
+    shell.print_plain("")
+    shell.print_plain('Use "exit -f" to force exit the Proton Framework.')
+    shell.print_plain("")
 
 def convert_to_parsable(obj):
     if isinstance(obj, dict):
@@ -29,8 +31,52 @@ def convert_to_parsable(obj):
 
     return new_obj
 
-
 def execute(shell, cmd):
+    sw = splitted[1].lower()
+    if sw == "-f":
+        restore_map = {}
+        restore_map['creds'] = convert_to_parsable(shell.creds)
+        restore_map['creds_keys'] = convert_to_parsable(shell.creds_keys)
+        restore_map['domain_info'] = convert_to_parsable(shell.domain_info)
+        restore_map['jobs'] = []
+        for jkey, j in shell.jobs.items():
+            new_j = {}
+            new_j['results'] = j.results
+            new_j['id'] = j.id
+            new_j['session_id'] = -1
+            new_j['completed'] = j.completed
+            new_j['ip'] = j.ip
+            new_j['name'] = j.name
+            new_j['key'] = j.key
+            restore_map['jobs'].append(new_j)
+
+        restore_map['sessions'] = []
+        for s in [vars(session) for skey, session in shell.sessions.items()]:
+            new_s = dict(s)
+            try:
+                new_s.pop('stager')
+                new_s.pop('shell')
+            except:
+                pass
+            new_s['status'] = 0
+            restore_map['sessions'].append(new_s)
+
+        blank_state = True
+
+        for k in restore_map:
+            if restore_map[k]:
+                blank_state = False
+
+        if not blank_state:
+            restore = open('restore.json', 'w')
+            import json
+            restore.write(json.dumps(restore_map)+"\n")
+            restore.close()
+
+        import sys
+        sys.exit(0)
+        
+
     conf = input('Exit Proton Framework? y/N: ')
     if conf == "y" or conf == "Y":
         time.sleep(0)
