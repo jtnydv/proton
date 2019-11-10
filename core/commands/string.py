383f6cf4f95183314831b65b3b4a92b531f483d6
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-10 05:48:57 -0500

    Update string.py

--- core/commands/string.py
+++ core/commands/string.py
@@ -5,13 +5,14 @@ def autocomplete(shell, line, text, state):
 
 def help(shell):
     shell.print_plain("")
-    shell.print_plain('Use "string %s" to string some text.' % (shell.colors.colorize("TEXT", shell.colors.BOLD)))
+    shell.print_plain('Use "string %s" to string the specified text.' % (shell.colors.colorize("TEXT", shell.colors.BOLD)))
     shell.print_plain("")
 
 def execute(shell, cmd):
     splitted = cmd.split()
     
     if len(splitted) > 1:
+        string = " ".join(cmd.split(" ")[1:])
         print(string)
     else:
 help(shell)
