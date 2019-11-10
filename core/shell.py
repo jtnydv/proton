06da73d114f5e1aaf37bb3002c623d3cb006b117
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-10 09:55:52 -0500

    Update shell.py

--- core/shell.py
+++ core/shell.py
@@ -50,10 +50,7 @@ class Shell(object):
 
                 cmd = ""
                 while len(autorun) > 0:
-                    auto = []
-                    for i in autorun:
-                        auto.append(i)
-                    cmd = auto.pop(0).split("#")[0].strip()
+                    cmd = autorun.pop(0).split("#")[0].strip()
                     if len(cmd) > 0:
                         break
 
