b8f1dd9ffa595758480f490c97632ff943469722
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-10 05:56:41 -0500

    Update print.py

--- core/commands/print.py
+++ core/commands/print.py
@@ -1,13 +1,13 @@
 import time
 
-DESCRIPTION = "String some text."
+DESCRIPTION = "Print some text."
 
 def autocomplete(shell, line, text, state):
     return None
 
 def help(shell):
     shell.print_plain("")
-    shell.print_plain('Use "string %s" to string the specified text.' % (shell.colors.colorize("TEXT", shell.colors.BOLD)))
+    shell.print_plain('Use "string %s" to print the specified text.' % (shell.colors.colorize("TEXT", shell.colors.BOLD)))
     shell.print_plain("")
 
 def execute(shell, cmd):
