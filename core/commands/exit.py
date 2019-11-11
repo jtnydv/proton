8ff9809357d8aa360daf1ebe1b93e5ff7f82c219
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-11 07:38:06 -0500

    Update exit.py

--- core/commands/exit.py
+++ core/commands/exit.py
@@ -32,6 +32,7 @@ def convert_to_parsable(obj):
     return new_obj
 
 def execute(shell, cmd):
+    splitted = cmd.split()
     sw = splitted[1].lower()
     if sw == "-f":
         restore_map = {}
