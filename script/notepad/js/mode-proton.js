0d4cab4aaf1a1c6f50f9f6868a96b78c8e219edd
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-14 20:52:58 +0300

    Add files via upload

---
+++ script/notepad/js/mode-proton.js
@@ -0,0 +1,70 @@
+define('ace/mode/duckycode', function(require, exports, module) {
+
+    var oop = require("ace/lib/oop");
+    var TextMode = require("ace/mode/text").Mode;
+    var ExampleHighlightRules = require("ace/mode/duckycode_highlight_rules").ExampleHighlightRules;
+    
+    var Mode = function() {
+        this.HighlightRules = ExampleHighlightRules;
+    };
+    oop.inherits(Mode, TextMode);
+
+
+    (function() {
+        // Extra logic goes here. (see below)
+    }).call(Mode.prototype);
+    
+    exports.Mode = Mode;
+    });
+    
+    define('ace/mode/duckycode_highlight_rules', function(require, exports, module) {
+    
+    var oop = require("ace/lib/oop");
+    var TextHighlightRules = require("ace/mode/text_highlight_rules").TextHighlightRules;
+    
+    var ExampleHighlightRules = function() {
+    
+        this.$rules = {
+         start: [{
+            token: 'entity.name.function',
+            regex: '^(VERBOSE|SPOOL|API|SOUNDS)'
+         },
+         {
+            token: 'string',
+            regex: '(^PYEXEC|#include <psio>)'
+         },
+         {
+            token: 'string',
+            regex: '(^PYEXEC|#include <psio>)'
+         },
+         {
+             token: 'comment',
+             regex: '(^#).*$'
+         },
+         {
+             token: 'constant.language',
+             regex: '^(JOBS|STAGERS|ZOMBIES|CREDS|DOMAIN|REPEAT)$'
+         },
+         {
+            token: 'support.type',
+            regex: '^(USE|SET|LOAD|BACK|DELAY)'
+         },
+         {
+            token: 'keyword',
+            regex: '^(UNSET|INFO|KILL)'
+         },
+         {
+            token: 'keywordMapper',
+            regex: "[a-zA-Z][a-zA-Z0-9_]*\\b"
+         },
+           ] 
+        }
+        
+    }
+    
+    oop.inherits(ExampleHighlightRules, TextHighlightRules);
+    
+    exports.ExampleHighlightRules = ExampleHighlightRules;
+    });
+    
+    
---
+++ script/notepad/notepad.py
@@ -0,0 +1,38 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+def is_keyword(os):
+    print(os)
+
+@app.route('/')
+def index():
+    text = open('index.html').read()
+    return text
+
+@app.route('/js/')
+def js():
+    return "protonscript.js\nmode-proton.js"
+
+@app.route('/css/')
+def css():
+    return "style.css"
+
+@app.route('/html/')
+def html():
+    return "index.html"
+
+@app.route('/css/style.css')
+def style():
+    return open('css/style.css').read()
+
+@app.route('/js/protonscript.js')
+def protonscript():
+    return open('js/protonscript.js').read()
+
+@app.route('/js/mode-proton.js')
+def mode_proton():
+    return open('js/mode-proton.js').read()
+
+if __name__ == '__main__':
+    app.run(debug=True)
