ff57a6b22eff813721e23f33992c9bdc1cc680bc
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-17 03:40:01 -0500

    Create int.py

---
+++ core/commands/int.py
@@ -0,0 +1,11 @@
+DESCRIPTION = "Reopen Proton Framework."
+
+def autocomplete(shell, line, text, state):
+    return None
+
+def help(shell):
+    pass
+
+def execute(shell, cmd):
+    import time
+    time.sleep(1)
