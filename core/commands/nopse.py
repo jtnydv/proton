b09eee0811a2e3778d2c309d2f0cc8713c91ced9
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-17 04:06:27 -0500

    Update nopse.py

--- core/commands/nopse.py
+++ core/commands/nopse.py
@@ -7,4 +7,25 @@ def help(shell):
     pass
 
 def execute(shell, cmd):
-    
+    import os.path
+        if (os.path.exists("/data/data/com.termux")):
+            pspath = "/data/data/com.termux/files/usr/bin/pscript"
+        else:
+            pspath = "/usr/local/bin/pscript"
+            
+        if (os.path.exists(pspath)):
+            import time
+            time.sleep(0)
+        else:
+            REDL="\033[1;31m"
+            WHSL="\033[0;97m"
+            ENDL="\033[0m"
+            print(REDL+"[-]"+ENDL+" "+WHSL+"ProtonScript is not installed!"+ENDL)
+            ips = input('Install ProtonScript y/N: ')
+            if ips == "y" or ips == "Y":
+                os.system("cd script && chmod +x install.sh && ./install.sh")
+                import sys
+                sys.exit()
+            else:
+                import sys
+                sys.exit()
