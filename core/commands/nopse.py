03e477bcd428d50f3e71517652d76e57b81c4ec1
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-17 04:12:27 -0500

    Update nopse.py

--- core/commands/nopse.py
+++ core/commands/nopse.py
@@ -7,7 +7,5 @@ def help(shell):
     pass
 
 def execute(shell, cmd):
-    REDL="\033[1;31m"
-    WHSL="\033[0;97m"
-    ENDL="\033[0m"
-    print(REDL+"[-]"+ENDL+" "+WHSL+"You can only run this through ProtonScript!"+ENDL)
+    import time
+    time.sleep(0)
