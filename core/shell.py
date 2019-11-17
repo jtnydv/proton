d0a286b74c0120753d20c7856c376ee1e1973553
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-17 04:49:04 -0500

    Update shell.py

--- core/shell.py
+++ core/shell.py
@@ -62,6 +62,7 @@ class Shell(object):
             try:
                 if len(autorun) > 0:
                     self.prompt = self.colors.get_prompt(self.state, False)
+                    self.clean_prompt = self.colors.get_prompt(self.state, False)
                     cmd = ''
                     while len(autorun) > 0:
                         cmd = autorun.pop(0).split("#")[0].strip()
