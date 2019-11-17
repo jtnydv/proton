385c2761a8890fd5e7dd7043b2866f60465b7b77
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-17 03:46:29 -0500

    Update shell.py

--- core/shell.py
+++ core/shell.py
@@ -43,7 +43,7 @@ class Shell(object):
         if restore_map:
             self.restore(restore_map)
         if len(autorun) > 0:
-            if autorun[-2][:3] == "INT":
+            if autorun[-1][:3] == "INT":
                 DEF = "DELAY 0"
             else:
                 DEF = "EXIT -f"
