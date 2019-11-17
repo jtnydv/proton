6cdfcd9fd96e785b880e7411dd91957a1c128714
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-17 06:06:12 -0500

    Update runner.py

--- script/runner/runner.py
+++ script/runner/runner.py
@@ -1,6 +1,10 @@
 import argparse
 import time
 import os
+
+G = "\033[1;32m"
+R = "\033[1;31m"
+E = "\033[0m"
     
 def decoder(pscode):
     binary = open(pscode).read()
@@ -29,19 +33,19 @@ if args.run:
         if '.' in args.run:
             time.sleep(0)
         else:
-            print("\n(1/3) Loading Program File ..... [ FAIL ]\n")
+            print("\n(1/3) Loading Program File ..... [ "+R+"FAIL"+E+" ]\n")
             import sys
             sys.exit()
             
         if (argrem[-(argrem[::-1].index('.')):]) == 'bin':
-            print("\n(1/3) Loading Program File  ..... [ OK ]")
+            print("\n(1/3) Loading Program File  ..... [ "+G+"OK"+E+" ]")
             
         else:
-            print("\n(1/3) Loading Program File ..... [ FAIL ]\n")
+            print("\n(1/3) Loading Program File ..... [ "+R+"FAIL"+E+" ]\n")
             import sys
             sys.exit()
     else:
-        print("\n(1/3) Loading Program File ..... [ FAIL ]\n")
+        print("\n(1/3) Loading Program File ..... [ "+R+"FAIL"+E+" ]\n")
         import sys
         sys.exit()
         
@@ -53,12 +57,12 @@ if args.run:
     time.sleep(1)
             
     if (os.path.exists(pspath)):
-        print("(2/3) Loading ProtonScript  ..... [ OK ]")
+        print("(2/3) Loading ProtonScript  ..... [ "+G+"OK"+E+" ]")
     else:
-        print("(2/3) Loading ProtonScript ..... [ FAIL ]\n")
+        print("(2/3) Loading ProtonScript ..... [ "+R+"FAIL"+E+" ]\n")
         import sys
         sys.exit()
     
-    print("(3/3) Running Program File  ..... [ OK ]")
+    print("(3/3) Running Program File  ..... [ "+G+"OK"+E+" ]")
     time.sleep(2)
     decoder(args.run)
