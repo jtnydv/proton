25d30aa3991d534c0992e756616013ce7e1aacaf
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-19 00:42:49 +0300

    Update repeat.py

--- core/commands/repeat.py
+++ core/commands/repeat.py
@@ -33,7 +33,7 @@ def print_all_repeatjobs(shell):
     shell.print_plain("")
 
     shell.print_plain(formats.format("ID", "NAME", "TTR", "CR", "TBR"))
-    shell.print_plain(formats.format("-"*2, "-"*4, "-"*5, "-"*3, "-"*5))
+    shell.print_plain(formats.format("-"*2, "-"*4, "-"*3, "-"*2, "-"*3))
     for rjob in shell.repeatjobs:
         rjobobj = shell.repeatjobs[rjob]
         shell.print_plain(formats.format(rjob, rjobobj[5], str(rjobobj[0]), str(rjobobj[1]-1), str(rjobobj[4])))
