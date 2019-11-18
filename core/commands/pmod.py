a53c0741e384f0c2b587ca74b9f8c3792808e842
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-18 19:51:08 +0300

    Update and rename pmod to pmod.py

---
+++ core/commands/pmod.py
@@ -0,0 +1,13 @@
+DESCRIPTION = "Print some text."
+
+def autocomplete(shell, line, text, state):
+    return None
+
+def help(shell):
+    pass
+
+def execute(shell, cmd):
+        COMMAND     DESCRIPTION     
+        ---------   -------------   
+        
+ 
