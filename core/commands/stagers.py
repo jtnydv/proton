65ba1983358ed885da441548e09d11f404c47d34
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-19 19:25:17 +0300

    Update stagers.py

--- core/commands/stagers.py
+++ core/commands/stagers.py
@@ -5,7 +5,7 @@ def autocomplete(shell, line, text, state):
 
 def help(shell):
     shell.print_plain("")
-    shell.print_plain('Use "stagers %s" to print a stager.' % shell.colors.colorize("STAGER_ID", [shell.colors.BOLD]))
+    shell.print_plain('Use "stagers %s" to display a stager.' % shell.colors.colorize("STAGER_ID", [shell.colors.BOLD]))
     shell.print_plain('Use "stagers -o %s" to print a stager\'s options.' % shell.colors.colorize("STAGER_ID", [shell.colors.BOLD]))
     shell.print_plain('Use "stagers -k %s" to kill a stager.' % shell.colors.colorize("STAGER_ID", [shell.colors.BOLD]))
     shell.print_plain("")
