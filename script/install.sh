d9c020a23269178de9f7da953b764cbde98890be
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-25 00:16:05 +0300

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -42,17 +42,19 @@ then
    exit
 fi
 
-if [[ -d ~/proton ]]
+if [[ -d /data/data/com.termux ]]
+then
+pspath="/data/data/com.termux/files/usr/bin/proton"
+else
+pspath="/usr/local/bin/proton"
+fi
+
+if [[ -f $pspath ]]
 then
   sleep 0
 else
-  cd ~
-  {
-  git clone https://github.com/entynetproject/proton.git
-  } &> /dev/null
-  cd ~/proton
-  chmod +x install.sh
-  ./install.sh
+  echo -e ""$RS"[-]"$WHS" Proton Framework is not installed!"$CE""
+  exit
 fi
 
 sleep 1
