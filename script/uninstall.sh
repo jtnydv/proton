d0f0f668866f3926f60fa82e6a7eb995b051e686
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-09 00:10:28 +0300

    Update uninstall.sh

--- script/uninstall.sh
+++ script/uninstall.sh
@@ -4,7 +4,7 @@
 #            --------------------------------------------------
 #                               ProtonScript               
 #            --------------------------------------------------
-#          Copyright (C) <2015>  <Entynetproject (Ivan Nikolsky)>
+#                  Copyright (C) <2019>  <Entynetproject>
 #
 #        This program is free software: you can redistribute it and/or modify
 #        it under the terms of the GNU General Public License as published by
@@ -21,7 +21,7 @@
 #
 #
 #    About Author :   
-#    Founder   : Entynetproject (Ivan Nikolsky)
+#    Founder   : Entynetproject
 #    Site      : http://entynetproject.simplesite.com/
 #    Instagram : @entynetproject 
 #    Twitter   : @entynetproject
