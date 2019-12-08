88cd4cf372d75c45f9778550c938a5faf94c19d7
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-09 00:07:34 +0300

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -4,7 +4,7 @@
 #            --------------------------------------------------
 #                               ProtonScript               
 #            --------------------------------------------------
-#          Copyright (C) <2015>  <Entynetproject (Ivan Nikolsky)>
+#                  Copyright (C) <2015>  <Entynetproject>
 #
 #        This program is free software: you can redistribute it and/or modify
 #        it under the terms of the GNU General Public License as published by
