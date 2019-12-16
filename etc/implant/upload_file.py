764636319002e02f192765af644ce9e16b81cd70
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-16 19:53:55 +0300

    Update upload_file.py

--- etc/implant/upload_file.py
+++ etc/implant/upload_file.py
@@ -54,10 +54,9 @@ class UploadFileImplant(core.implant.Implant):
         os.chdir(w)  
         
         #payloads["vbs"] = self.load_script("data/implant/util/upload_file.vbs", self.options)
-        print(os.environ['PWD'])
         payloads["js"] = os.environ['HOME'] + "/proton/data/implant/util/upload_file.js"
+        
+        self.dispatch(payloads, self.job)
 
         g = os.environ['HOME']
         os.chdir(g + "/proton")
-        
-        self.dispatch(payloads, self.job)
