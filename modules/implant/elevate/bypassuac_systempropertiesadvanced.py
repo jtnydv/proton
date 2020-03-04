8ba826b02181c3dd6c7c6f93078e90e1fba5844d
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-03-05 01:43:07 +0300

    Update bypassuac_systempropertiesadvanced.py

--- modules/implant/elevate/bypassuac_systempropertiesadvanced.py
+++ modules/implant/elevate/bypassuac_systempropertiesadvanced.py
@@ -27,7 +27,7 @@ class SystemPropertiesAdvancedJob(core.job.Job):
         if self.session_id == -1:
             return
         if (int(self.session.build) < 14393 or int(self.session.build) >= 18632) and self.options.get("IGNOREBUILD") == "false":
-            self.error("0", "The target may not be vulnerable to this implant. Set IGNOREBUILD to true to run anyway.", "Target build is not vulnerable!", "")
+            self.error("0", "The target may not be vulnerable to this implant. Set IGNOREBUILD to true to run anyway.", "Target build is not vulnerable.", "")
             return False
 
     def done(self):
