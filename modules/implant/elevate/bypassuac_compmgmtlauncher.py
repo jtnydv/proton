0d4d73f1b7dc1abf851c59301032427e15fc63e8
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-03-05 01:44:42 +0300

    Update bypassuac_compmgmtlauncher.py

--- modules/implant/elevate/bypassuac_compmgmtlauncher.py
+++ modules/implant/elevate/bypassuac_compmgmtlauncher.py
@@ -30,7 +30,7 @@ class CompMgmtLauncherJob(core.job.Job):
         if self.session_id == -1:
             return
         if (int(self.session.build) < 7600 or int(self.session.build) > 15030) and self.options.get("IGNOREBUILD") == "false":
-            self.error("0", "The target may not be vulnerable to this implant. Set IGNOREBUILD to true to run anyway.", "Target build not vulnerable.", "")
+            self.error("0", "The target may not be vulnerable to this implant. Set IGNOREBUILD to true to run anyway.", "Target build is not vulnerable.", "")
             return False
 
     def done(self):
